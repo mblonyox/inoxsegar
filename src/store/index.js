7bd17e4e187c789f8ba6a89501779f4cc7760db5
mblonyox <mblonyox@gmail.com>
2017-11-30 07:17:58 +0700

    Mommentary cleanup & install bulma, vuex, persistedstate

---
+++ src/store/index.js
@@ -0,0 +1,11 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+import createPersistedState from 'vuex-persistedstate'
+
+Vue.use(Vuex)
+
+const store = new Vuex.Store({
+  plugins: [createPersistedState()]
+})
+
+export default store
