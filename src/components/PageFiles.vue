81f2d10bdeca65e3b75e4b7ab198471b51e8d286
mblonyox <mblonyox@gmail.com>
2017-12-21 23:18:21 +0700

    Page Movie first baby step.

--- src/components/PageFiles.vue
+++ src/components/PageFiles.vue
@@ -1,52 +1,50 @@
 <template>
-  <div id="uploads">
-    <section class="container">
-      <h1 class="title">File yang diunggah.</h1>
-      <h2 class="subtitle">Menampilkan file-file yang diunggah oleh semua pengguna. File yang telah dimasukkan dalam koleksi juga dapat diakses dari laman koleksi tersebut.</h2>
-      <hr>
-      <div class="box">
-        <table class="table is-fullwidth">
-          <thead>
-            <tr>
-              <th>#</th>
-              <th>Nama File</th>
-              <th>Ukuran</th>
-              <th>Uploader</th>
-              <th>Tanggal</th>
-              <th>Koleksi</th>
-              <th>Aksi</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr v-for="file in files" :key="file.id">
-              <td>{{ files.indexOf(file) + 1}}</td>
-              <td>{{ file.name }}</td>
-              <td>{{ humanFilesize(file.size) }}</td>
-              <td>{{ file.uploader }}</td>
-              <td>{{ (new Date(file.date)).toLocaleString('id-ID') }}</td>
-              <td>{{ file.koleksi ? file.kolesi.title : 'Uncategorized'}}</td>
-              <td>
-                <span class="icon">
-                  <i class="fa fa-thumbs-o-up"></i>
+  <section id="files" class="container">
+    <h1 class="title">File yang diunggah.</h1>
+    <h2 class="subtitle">Menampilkan file-file yang diunggah oleh semua pengguna. File yang telah dimasukkan dalam koleksi juga dapat diakses dari laman koleksi tersebut.</h2>
+    <hr>
+    <div class="box">
+      <table class="table is-fullwidth">
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Nama File</th>
+            <th>Ukuran</th>
+            <th>Uploader</th>
+            <th>Tanggal</th>
+            <th>Koleksi</th>
+            <th>Aksi</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="file in files" :key="file.id">
+            <td>{{ files.indexOf(file) + 1}}</td>
+            <td>{{ file.name }}</td>
+            <td>{{ humanFilesize(file.size) }}</td>
+            <td>{{ file.uploader }}</td>
+            <td>{{ (new Date(file.date)).toLocaleString('id-ID') }}</td>
+            <td>{{ file.koleksi ? file.kolesi.title : 'Uncategorized'}}</td>
+            <td>
+              <span class="icon">
+                <i class="fa fa-thumbs-o-up"></i>
+              </span>
+              <span class="icon">
+                <i class="fa fa-thumbs-o-down"></i>
+              </span>
+              <a :href="serverUrl + file.uploaded_path + '/' + file.id + '/' + file.name">
+                <span>
+                  <i class="fa fa-download"></i>
                 </span>
-                <span class="icon">
-                  <i class="fa fa-thumbs-o-down"></i>
-                </span>
-                <a :href="serverUrl + file.uploaded_path + '/' + file.id + '/' + file.name">
-                  <span>
-                    <i class="fa fa-download"></i>
-                  </span>
-                </a>
-                <span class="icon" v-if="$store.state.auth.admin">
-                  <i class="fa fa-wrench"></i>
-                </span>
-              </td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
-    </section>
-  </div>
+              </a>
+              <span class="icon" v-if="$store.state.auth.admin">
+                <i class="fa fa-wrench"></i>
+              </span>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </section>
 </template>
 
 <script>
@@ -89,7 +87,7 @@ export default {
 </script>
 
 <style>
-#uploads .container {
+#files {
   margin-top: 3rem;
 }
 </style>
--- src/components/PageMovie.vue
+++ src/components/PageMovie.vue
@@ -1,3 +1,35 @@
 <template>
-  <h1>Page Movie</h1>
+  <section id="movies" class="container">
+    <h1 class="title">Koleksi Movies</h1>
+    <div class="box">
+      <div class="columns is-multiline">
+        <div class="column is-one-quarter" v-for="movie in movies">
+          <div class="card">
+            <div class="card-image">
+              <figure class="image">
+                <img :src="movie.Poster">
+              </figure>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
+
+<script>
+import {Search} from '../assets/movies.json'
+export default {
+  data () {
+    return {
+      movies: Search
+    }
+  }
+}
+</script>
+
+<style>
+  #movies {
+    margin-top: 3rem;
+  }
+</style>
