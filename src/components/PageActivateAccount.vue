862e8cabacf4fc8bd8d6814418c36e02e705f0fd
mblonyox <mblonyox@gmail.com>
2018-04-23 00:19:39 +0700

    Ubah pakai apiservice, fixing request email tambahan.

--- src/components/PageActivateAccount.vue
+++ src/components/PageActivateAccount.vue
@@ -7,7 +7,7 @@
         <img :src="avatar">
       </figure>
       <p class="subtitle has-text-black">Kode Aktivasi telah dikirim ke email anda: <a :href="'mailto:'+email" class="has-text-info">{{ email }}</a>.</p>
-      <p class="subtitle has-text-black">Belum menerima email? <a class="has-text-info" @click="resendActivation" v-if="!sendingMail">Kirim Ulang</a> <span v-else><i class="fa fa-spinner fa-pulse fa-fw"></i></span></p>
+      <p class="subtitle has-text-black">Belum menerima email? <span v-if="mailSent" class="has-text-success">Email tambahan terkirim. <i class="fa fa-check-circle-o"></i> </span> <span v-else-if="pending"><i class="fa fa-spinner fa-pulse fa-fw"></i></span> <a class="has-text-info" @click="resendActivation" v-else>Kirim Ulang</a></p>
       <hr>
       <form @submit.prevent="activate">
         <div class="control has-icons-left">
@@ -16,10 +16,6 @@
             <i class="fa fa-key"></i>
           </span>
           <p class="help is-danger has-text-left" v-if="!isValid.kode">{{ isValid.kodeHelper }}</p>
-        </div>
-        <div class="notification is-danger" v-if="error">
-          <button class="delete" @click.prevent="closeError"></button>
-          <span>{{ error }}</span>
         </div>
         <button type="submit" class="button is-block is-info is-large" :class="{'is-loading': pending}">
           <span>Aktivasi</span>
@@ -35,7 +31,7 @@
 <script>
 import md5 from 'md5'
 import defaultAvatar from '../assets/mblonyox-logo-sm.png'
-import serverUrl from '../helpers/backend-url'
+import { WithToken } from '../helpers/api-service'
 
 export default {
   data () {
@@ -45,7 +41,7 @@ export default {
         kode: null,
         kodeHelper: ''
       },
-      sendingMail: false
+      mailSent: false
     }
   },
   computed: {
@@ -61,10 +57,7 @@ export default {
       return this.$store.state.auth.user.email
     },
     pending () {
-      return this.$store.state.auth.pending
-    },
-    error () {
-      return this.$store.state.auth.error
+      return this.$store.state.ui.pending
     }
   },
   methods: {
@@ -75,32 +68,14 @@ export default {
       this.$store.dispatch('activate', this.kode)
     },
     resendActivation () {
-      this.sendingMail = true
-      let httpHeaders = new Headers({
-        'x-access-token': this.$store.state.auth.token
+      WithToken.doRequest({
+        url: 'resend_activation'
       })
-      fetch(serverUrl + 'api/resend_activation', {
-        method: 'GET',
-        mode: 'cors',
-        headers: httpHeaders
-      })
-        .then(response => response.json())
-        .then(data => {
-          this.sendingMail = false
-          if (data.success) {
-            this.$notify({
-              group: 'system',
-              title: 'Email aktivasi.',
-              text: data.message,
-              type: 'success'
-            })
-          } else {
-            this.$store.commit('setAuthError', data.message)
+        .then(state => state.result.body)
+        .then(body => {
+          if (body.success) {
+            this.mailSent = true
           }
-        })
-        .catch(err => {
-          this.sendingMail = false
-          this.$store.commit('setAuthError', err.message)
         })
     },
     closeError () {
