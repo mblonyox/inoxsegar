3a447b4d79c40889512da3faf9694a8c06bc16c0
mblonyox <mblonyox@gmail.com>
2018-05-04 06:34:12 +0700

    Fixing UglifyJsPlugin.

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -13,6 +13,7 @@ const ExtractTextPlugin = require('extract-text-webpack-plugin')
 const OptimizeCSSPlugin = require('optimize-css-assets-webpack-plugin')
 const SWPrecacheWebpackPlugin = require('sw-precache-webpack-plugin')
 const loadMinified = require('./load-minified')
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
 
 const env = config.build.env
 
@@ -34,12 +35,13 @@ const webpackConfig = merge(baseWebpackConfig, {
     new webpack.DefinePlugin({
       'process.env': env
     }),
-    new webpack.optimize.UglifyJsPlugin({
-      compress: {
-        warnings: false
-      },
-      sourceMap: true
-    }),
+    // new webpack.optimize.UglifyJsPlugin({
+    //   compress: {
+    //     warnings: false
+    //   },
+    //   sourceMap: true
+    // }),
+    new UglifyJsPlugin(),
     // extract css into its own file
     new ExtractTextPlugin({
       filename: utils.assetsPath('css/[name].[contenthash].css')
