40538116624141284fc058d1c5820bcad176e49b
mblonyox <mblonyox@gmail.com>
2018-07-16 07:03:03 +0700

    Ubah laman stats.

--- src/components/PageStats.vue
+++ src/components/PageStats.vue
@@ -1,84 +1,46 @@
 <template>
   <div id="stats" style="margin-top: 3em">
     <div class="container">
-      <div v-if="isAdmin">
-        <h1 class="title">Log Download</h1>
-        <hr>
-        <div class="box">
-          <table class="table is-fullwidth">
-            <thead>
-              <tr>
-                <th>Waktu</th>
-                <th>User</th>
-                <th>File</th>
-                <th>Ukuran</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr v-for="log in logs" :key="log._id">
-                <td>{{ (new Date(log.createdAt)).toLocaleString('id-ID') }}</td>
-                <td><router-link :to="{name: 'PageUser', params: {id: log.user._id }}" v-if="log.user">{{ log.user.username }}</router-link> </td>
-                <td>{{ log.target.name }}</td>
-                <td>{{ humanFilesize(log.target.size) }}</td>
-              </tr>
-              <tr>
-                <td colspan="4">
-                  <infinite-loading @infinite="infiniteHandler" />
-                </td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
+      <div class="tabs is-centered is-boxed is-fullwidth">
+        <ul>
+          <li
+            v-for="tab in tabs"
+            :class="{'is-active': activeTab === tab}">
+            <a @click="activeTab = tab">{{ tab }}</a>
+          </li>
+        </ul>
       </div>
-      <div v-else>
-        <h1 class="title"> Halaman ini hanya untuk admin.</h1>
+      <div class="box" v-if="activeTab === 'Top'">
+
+      </div>
+      <div class="box" v-if="activeTab === 'Downloads'">
+        <log-download v-if="isAdmin"/>
+        <h1 v-else>Hanya admin.</h1>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import InfiniteLoading from 'vue-infinite-loading'
-import { NoNotify } from '../helpers/api-service'
-import filesize from 'filesize'
+import LogDownload from './StatsLogDownload'
 
 export default {
   data: () => ({
-    logs: [],
-    page: 0
+    tabs: [
+      'Top',
+      'Downloads',
+      'Uploads',
+      'Users'
+    ],
+    activeTab: 'Top'
   }),
   computed: {
     isAdmin() {
       return this.$store.state.auth.user.admin
     }
   },
-  methods: {
-    humanFilesize(bytes) {
-      return filesize(bytes)
-    },
-    infiniteHandler($state) {
-      this.page++
-      NoNotify.doRequest({
-        url: 'stats/log_download',
-        query: {
-          page: this.page
-        }
-      })
-        .then(status => status.result.body)
-        .then(body => {
-          if (body.success) {
-            this.logs = this.logs.concat(body.data.logs)
-            $state.loaded()
-            if (body.data.logs.length < 100) {
-              $state.complete()
-            }
-          }
-        })
-    }
-  },
   components: {
-    InfiniteLoading
+    LogDownload
   }
 }
 </script>
-
